001e1652ad7d9f693cd7225d4cfd5e2fb9754027
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2024-03-20 14:51:26 +0100

    Update business.js

--- routes/business.js
+++ routes/business.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "../uploads" });
+const upload = multer({ dest: "./util/images" });
 
 const businessController = require("../controller/business");
 
