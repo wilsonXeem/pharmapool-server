348f22e6717c6d21bac39e6daf01e0708da65ee4
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2024-03-20 15:03:37 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,32 +6,14 @@ const authController = require("../controller/auth");
 
 router.post(
   "/signup",
-  [
-    body("lastName", "last name should not be empty").not().isEmpty(),
-    body("email", "email is invalid").isEmail().not().isEmpty(),
-    body("password", "password should not be less than 8 characters long")
-      .isLength({ min: 8 })
-      .not()
-      .isEmpty(),
-    body("phoneNumber", "number should not be empty").not().isEmpty(),
-    body("gender", "select a gender").not().isEmpty(),
-  ],
   authController.userSignup
 );
 router.post(
   "/signin",
-  [
-    body("email", "email is invalid").isEmail().not().isEmpty(),
-    body("password", "password should not be less than 8 characters long")
-      .isLength({ min: 8 })
-      .not()
-      .isEmpty(),
-  ],
   authController.userLogin
 );
 router.post(
   "/password-reset",
-  [body("email", "Please input a valid email").isEmail().not().isEmpty()],
   authController.passwordReset
 );
 router.get("/password-reset/:email", authController.getPasswordToken);
