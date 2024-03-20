4a983a673a1f69e463cc497b37682591a0efd0e5
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2024-03-20 14:51:46 +0100

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const multer = require("multer")
 
-const upload = multer({ dest: "../uploads" });
+const upload = multer({ dest: "./util/images" });
 
 const profileController = require("../controller/profile")
 
