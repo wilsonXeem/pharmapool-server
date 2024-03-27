31fab9cf99a35c9950ae0d653cd202d86598938f
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2024-03-28 00:28:30 +0100

    Update post.js

--- model/post.js
+++ model/post.js
@@ -12,11 +12,6 @@ const postSchema = new Schema(
         type: Schema.Types.ObjectId,
       },
     },
-    product: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "Product",
-    },
     edited: Date,
     creator: {
       type: Schema.Types.ObjectId,
